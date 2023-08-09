7aaac332c93e184445c125e9dd552a14b8c06efa
sm-oh <whiteosm@naver.com>
2023-08-09 11:14:00 +0900

    캐스퍼

--- script/main.js
+++ script/main.js
@@ -18,4 +18,51 @@ $(document).ready(function(){
 //이런경우는 함수의 마지막에 return false를 넣어주는 방법과
 //e(이벤트 객체)를 사용하여 e.preventDefault();를 작성해주는 방법이 있다.
 
+
+// 이벤트 배너 구현
+  const e_banner = $('.e_banner > ul');
+  const c_btn = $('.e_banner .ctrl_btn span');
+  let i = $('.e_banner .ctrl_btn span').index();
+  let e_w = $('.e_banner').width(); //1200
+
+  console.log(i); //0
+
+  function moveLeft(){
+    if(i==3){
+      i=0;
+    }else{
+      i++;
+    }
+    let mv = -(e_w * i);
+    console.log(mv); //-0 -1200, -2400, -3600
+    e_banner.animate({'left':mv},500);
+
+    c_btn.removeClass('act'); //콘트롤버튼 서식 모두제거하고
+    $('.ctrl_btn span').eq(i).addClass('act'); //해당하는 콘트롤 버튼 서식에 적용.
+  }
+
+  //1. 콘트롤 버튼을 클릭하면 해당 이미지가 나오게
+  c_btn.click(function(){ //콘트롤 버튼 클릭시
+
+    i = $(this).index(); //해당 인덱스값을 변수에 담아서
+
+    clearInterval(Timer); //자동으로 움직이는 것을 막는다.
+    
+    let mv = -(e_w * i);
+    console.log(mv); //-0 -1200, -2400, -3600
+    e_banner.animate({'left':mv},500);
+
+    c_btn.removeClass('act');
+    $(this).addClass('act');
+  });
+
+  //2. 자동으로 움직이게 시간객체를 사용
+  let Timer = setInterval(moveLeft, 3000);
+
+  //3. 콘트롤 버튼에 클릭시 시간을 제거하여 멈추게하고 마우스 오버시 다시 시간을 재생하여 움직이게 한다.
+  c_btn.mouseleave(function(){
+    clearInterval(Timer);
+    Timer = setInterval(moveLeft, 3000);
+  });
+
 });
